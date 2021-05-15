304fb729ae06d69c99cb62dfdb1815ec710af1c3
Jaspier <57241026+Jaspier@users.noreply.github.com>
2021-05-15 15:36:00 +0100

    tested user registration end point with validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,28 @@
 import express from 'express';
 const usersRouter = express.Router();
-import { check, validationResult } from 'express-validator/check';
+import pkg from 'express-validator/check/index.js';
+const { check, validationResult } = pkg;
 
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
-usersRouter.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User route');
-});
+usersRouter.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); // returns error messages
+    }
+    res.send('User route');
+  }
+);
 
 export default usersRouter;
