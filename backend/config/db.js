7aff9ab3db453e68a668f8a7092717c70f150f10
Jaspier <57241026+Jaspier@users.noreply.github.com>
2021-05-15 14:18:22 +0100

    connecting to the database

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import config from 'config';
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MondoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import connectDB from './config/db.js';
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
